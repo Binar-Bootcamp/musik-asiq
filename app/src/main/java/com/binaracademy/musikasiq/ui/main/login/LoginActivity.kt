30f1e989444a1711c6e7784bf7f2b439e8c271b0
jlianf <jalianfebriandy15@gmail.com>
2023-03-16 00:48:59 +0700

    AND-7 and AND-8

---
+++ app/src/main/java/com/binaracademy/musikasiq/ui/main/login/LoginActivity.kt
@@ -0,0 +1,16 @@
+package com.binaracademy.musikasiq.ui.main.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.binaracademy.musikasiq.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    private val binding : ActivityLoginBinding by lazy {
+        ActivityLoginBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
---
+++ app/src/main/java/com/binaracademy/musikasiq/ui/main/register/RegisterActivity.kt
@@ -0,0 +1,18 @@
+package com.binaracademy.musikasiq.ui.main.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.binaracademy.musikasiq.databinding.ActivityRegisterBinding
+
+class RegisterActivity : AppCompatActivity() {
+    private val binding : ActivityRegisterBinding by lazy {
+        ActivityRegisterBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+
+    }
+}
