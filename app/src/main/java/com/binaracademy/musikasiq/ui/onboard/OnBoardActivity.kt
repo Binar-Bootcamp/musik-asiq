d5309d8ed1bfd44ef24c8df685646e7593ad5ecd
Ryan Aprianto <ryan.aprianto77777@gmail.com>
2023-03-26 00:37:01 +0700

    Hide Statur Bar in OnBoarding

--- app/src/main/java/com/binaracademy/musikasiq/ui/onboard/OnBoardActivity.kt
+++ app/src/main/java/com/binaracademy/musikasiq/ui/onboard/OnBoardActivity.kt
@@ -1,14 +1,14 @@
 package com.binaracademy.musikasiq.ui.onboard
 
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
-import android.widget.MediaController
+import android.view.WindowInsets
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.binaracademy.musikasiq.R
 import com.binaracademy.musikasiq.databinding.ActivityOnboardBinding
 import com.binaracademy.musikasiq.ui.login.LoginActivity
-import com.binaracademy.musikasiq.ui.main.MainActivity
-import com.binaracademy.musikasiq.ui.register.RegisterActivity
 import com.binaracademy.musikasiq.utils.helpers.intentTo
 
 class OnBoardActivity : AppCompatActivity() {
@@ -19,6 +19,16 @@ class OnBoardActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(binding.root)
+		
+		// Hide the status bar
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+			window.insetsController?.hide(WindowInsets.Type.statusBars())
+		} else {
+			window.setFlags(
+				WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN
+			)
+		}
 		
 		binding.videoView.setMediaController(null)
 		binding.videoView.setVideoURI(Uri.parse("android.resource://" + packageName + "/" + R.raw.onboarding))
