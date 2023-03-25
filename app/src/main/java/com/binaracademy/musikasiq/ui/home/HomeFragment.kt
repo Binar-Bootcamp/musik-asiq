1f1a229d62c834de82018a6c247b658c9cfe9b66
Ryan Aprianto <ryan.aprianto77777@gmail.com>
2023-03-25 17:57:32 +0700

    Adjust OnBoarding and Add Shimmer

--- app/src/main/java/com/binaracademy/musikasiq/ui/home/HomeFragment.kt
+++ app/src/main/java/com/binaracademy/musikasiq/ui/home/HomeFragment.kt
@@ -11,49 +11,62 @@ import com.binaracademy.musikasiq.databinding.FragmentHomeBinding
 import com.binaracademy.musikasiq.utils.hideSoftKeyboard
 import com.binaracademy.musikasiq.utils.load
 import com.binaracademy.musikasiq.viewmodel.HomeViewModel
-
+import com.facebook.shimmer.ShimmerFrameLayout
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class HomeFragment : Fragment() {
-
-    private var _binding: FragmentHomeBinding? = null
-
-    private val binding get() = _binding!!
-
-    private val popularAdapter = MostPopularAdapter()
-
-    private val viewModel: HomeViewModel by viewModels()
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        binding.root.setOnClickListener { it.hideSoftKeyboard() }
-        binding.imgViewAvatar.load("https://ui-avatars.com/api/?name=Budi Rahmawan&size=528.svg")
-        viewModel.loadPopularTracks(null)
-
-        viewModel.getPopularTracks().observe(viewLifecycleOwner){
-            it.onSuccess { response ->
-                popularAdapter.updatePopular(ArrayList(response.tracks.items))
-            }
-        }
-        setupRecyclerView()
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-
-    private fun setupRecyclerView() {
-        binding.rvListMostPopular.apply {
-            adapter = popularAdapter
-            layoutManager = GridLayoutManager(activity, 2)
-        }
-    }
+	
+	private var _binding: FragmentHomeBinding? = null
+	
+	private val binding get() = _binding!!
+	
+	private val popularAdapter = MostPopularAdapter()
+	
+	private val viewModel: HomeViewModel by viewModels()
+	
+	private lateinit var shimmer: ShimmerFrameLayout
+	
+	override fun onCreateView(
+		inflater: LayoutInflater, container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View {
+		_binding = FragmentHomeBinding.inflate(inflater, container, false)
+		return binding.root
+	}
+	
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+		binding.root.setOnClickListener { it.hideSoftKeyboard() }
+		binding.imgViewAvatar.load("https://ui-avatars.com/api/?name=Budi Rahmawan&size=528.svg")
+		viewModel.loadPopularTracks(null)
+		
+		binding.shimmerViewContainer.startShimmer()
+		
+		viewModel.getPopularTracks().observe(viewLifecycleOwner) {
+			it.onSuccess { response ->
+				binding.shimmerViewContainer.stopShimmer()
+				binding.shimmerViewContainer.visibility = View.GONE
+				popularAdapter.updatePopular(ArrayList(response.tracks.items))
+			}
+			it.onFailure {
+				binding.shimmerViewContainer.stopShimmer()
+				binding.shimmerViewContainer.visibility = View.GONE
+			}
+		}
+		setupRecyclerView()
+	}
+	
+	override fun onDestroyView() {
+		super.onDestroyView()
+		_binding = null
+	}
+	
+	private fun setupRecyclerView() {
+		binding.rvListMostPopular.apply {
+			adapter = popularAdapter
+			layoutManager = GridLayoutManager(activity, 2)
+		}
+	}
 }
--- app/src/main/java/com/binaracademy/musikasiq/ui/onboard/OnBoardActivity.kt
+++ app/src/main/java/com/binaracademy/musikasiq/ui/onboard/OnBoardActivity.kt
@@ -3,49 +3,30 @@ package com.binaracademy.musikasiq.ui.onboard
 import android.net.Uri
 import android.os.Bundle
 import android.widget.MediaController
-import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
+import com.binaracademy.musikasiq.R
 import com.binaracademy.musikasiq.databinding.ActivityOnboardBinding
 import com.binaracademy.musikasiq.ui.main.MainActivity
 import com.binaracademy.musikasiq.utils.helpers.intentTo
 
 class OnBoardActivity : AppCompatActivity() {
-    private val binding: ActivityOnboardBinding by lazy {
-        ActivityOnboardBinding.inflate(layoutInflater)
-    }
-
-    var Videoview: VideoView? = null
-    var mediaControls: MediaController? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(com.binaracademy.musikasiq.R.layout.activity_onboard)
-
-        Videoview = findViewById<VideoView>(com.binaracademy.musikasiq.R.id.videoView) as VideoView
-
-        if (mediaControls == null) {
-            mediaControls = MediaController(this)
-
-            mediaControls!!.setAnchorView(this.Videoview)
-        }
-
-        Videoview!!.setMediaController(mediaControls)
-        Videoview!!.setVideoURI(Uri.parse("android.resource://" + packageName + "/" + com.binaracademy.musikasiq.R.raw.onboarding))
-        Videoview!!.requestFocus()
-        Videoview!!.start()
-
-        setVideoViewToLoop(Videoview!!)
-
-        binding.apply {
-            btnGetstarted.setOnClickListener{
-                intentTo(MainActivity::class.java)
-            }
-        }
-    }
-
-    private fun setVideoViewToLoop(Videoview : VideoView){
-        Videoview.setOnCompletionListener {
-            Videoview.start()
-        }
-    }
+	private val binding: ActivityOnboardBinding by lazy {
+		ActivityOnboardBinding.inflate(layoutInflater)
+	}
+	
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(binding.root)
+		
+		binding.videoView.setMediaController(null)
+		binding.videoView.setVideoURI(Uri.parse("android.resource://" + packageName + "/" + R.raw.onboarding))
+		binding.videoView.requestFocus()
+		binding.videoView.start()
+		
+		binding.apply {
+			btnGetStarted.setOnClickListener {
+				intentTo(MainActivity::class.java)
+			}
+		}
+	}
 }
