9f3e6f9b8359259ff06558395df280a1449b30a6
Ryan Aprianto <ryan.aprianto77777@gmail.com>
2023-03-25 18:17:40 +0700

    Add Action for Input Search

---
+++ app/src/main/java/com/binaracademy/musikasiq/ui/result/ResultActivity.kt
@@ -0,0 +1,12 @@
+package com.binaracademy.musikasiq.ui.result
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.binaracademy.musikasiq.R
+
+class ResultActivity : AppCompatActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_result)
+	}
+}
