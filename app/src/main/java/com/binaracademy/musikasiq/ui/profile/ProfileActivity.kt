51918d66649c9b41361bdc6d1853d687a7fbd51d
Mochammad Kurnia Febrianto <mkurniafebrianto@192.168.1.7>
2023-03-26 19:42:28 +0700

    adjust profile activity

--- app/src/main/java/com/binaracademy/musikasiq/ui/profile/ProfileActivity.kt
+++ app/src/main/java/com/binaracademy/musikasiq/ui/profile/ProfileActivity.kt
@@ -6,6 +6,7 @@ import com.binaracademy.musikasiq.databinding.ActivityProfileBinding
 import com.binaracademy.musikasiq.databinding.ActivityRegisterBinding
 import com.binaracademy.musikasiq.ui.home.HomeFragment
 import com.binaracademy.musikasiq.ui.login.LoginActivity
+import com.binaracademy.musikasiq.ui.main.MainActivity
 import com.binaracademy.musikasiq.utils.helpers.Constants
 import com.binaracademy.musikasiq.utils.helpers.SharedPreferencesManager
 import com.binaracademy.musikasiq.utils.helpers.intentTo
@@ -24,19 +25,21 @@ class ProfileActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val getUsername = sharedPreferences.getString(Constants.NAME_SP_KEY, "Guest")
+        val getEmail = sharedPreferences.getString(Constants.EMAIL_SP_KEY, "root@example.com")
         binding.tvName.text = getUsername
-        binding.tvUsername.text = getUsername
+        binding.tvUsername.text = getEmail
 
         binding.photoProfile.load(
             "https://ui-avatars.com/api/?name=$getUsername&size=528.svg"
         )
 
         binding.btnBack.setOnClickListener {
-            intentTo(HomeFragment::class.java)
+            intentTo(MainActivity::class.java)
         }
 
         binding.btnLogout.setOnClickListener {
             sharedPreferences.remove(Constants.NAME_SP_KEY)
+            sharedPreferences.remove(Constants.EMAIL_SP_KEY)
             intentTo(LoginActivity::class.java)
         }
     }
