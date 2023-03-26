a5392d3131a303affed50e71cc33e764dd82b2ca
Mochammad Kurnia Febrianto <mkurniafebrianto@192.168.1.7>
2023-03-26 19:34:20 +0700

    add logic to profile and fix layout

--- app/src/main/java/com/binaracademy/musikasiq/ui/profile/ProfileActivity.kt
+++ app/src/main/java/com/binaracademy/musikasiq/ui/profile/ProfileActivity.kt
@@ -4,8 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.binaracademy.musikasiq.databinding.ActivityProfileBinding
 import com.binaracademy.musikasiq.databinding.ActivityRegisterBinding
+import com.binaracademy.musikasiq.ui.home.HomeFragment
+import com.binaracademy.musikasiq.ui.login.LoginActivity
+import com.binaracademy.musikasiq.utils.helpers.Constants
+import com.binaracademy.musikasiq.utils.helpers.SharedPreferencesManager
+import com.binaracademy.musikasiq.utils.helpers.intentTo
+import com.binaracademy.musikasiq.utils.load
 
 class ProfileActivity : AppCompatActivity() {
+
+    private val sharedPreferences = SharedPreferencesManager(this, Constants.APP_TABLE)
+
     private val binding : ActivityProfileBinding by lazy {
         ActivityProfileBinding.inflate(layoutInflater)
     }
@@ -14,5 +23,21 @@ class ProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        val getUsername = sharedPreferences.getString(Constants.NAME_SP_KEY, "Guest")
+        binding.tvName.text = getUsername
+        binding.tvUsername.text = getUsername
+
+        binding.photoProfile.load(
+            "https://ui-avatars.com/api/?name=$getUsername&size=528.svg"
+        )
+
+        binding.btnBack.setOnClickListener {
+            intentTo(HomeFragment::class.java)
+        }
+
+        binding.btnLogout.setOnClickListener {
+            sharedPreferences.remove(Constants.NAME_SP_KEY)
+            intentTo(LoginActivity::class.java)
+        }
     }
 }
